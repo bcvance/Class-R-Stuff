c9acdd14e4561c5873e4a6626b3f7f733a1f2bb5
bcvance <ben@vancenet.net>
2026-04-02 13:58:17 -0500

    3/30

--- R Practice/3:25 R Practice.R
+++ R Practice/3:25 R Practice.R
@@ -23,3 +23,32 @@ m <- lm(ldt_remove$Mean_RT ~ ldt_remove$Length)
 
 abline(m)
 
+par(mfrow = c(1,2))
+
+plot(density(ldt_remove$Length),
+     main = "Density of Word Length", xlab = "Mean RT")
+
+par(mfrow = c(1,1))
+
+ncvTest(m)
+
+durbinWatsonTest(m)
+
+cor.test(ldt_remove$Length, ldt_remove$Mean_RT,
+         method = "pearson", alternative = "greater")
+
+lex <- c(47, 89, 131, 186, 245, 284, 362, 444, 553, 627)
+
+gram <- c(0, 2, 1, 3, 5, 9, 7, 16, 25, 34)
+
+child <- data.frame(lex, gram)
+
+str(child)
+
+plot(child$lex, child$gram,
+     main = "Scatterplot of Vocabulary Size and Grammatical Complexity",
+     xlab = "Lexical Unit", ylab = "Grammatical Complexity Score")
+
+cor.test(child$lex, child$gram, method = "spearman", alternative = "greater")
+
+cor.test(child$lex, child$gram, method = "kendall", alternative = "greater")
---
+++ R Practice/R Practice 3:30.R
@@ -0,0 +1,44 @@
+library(car)
+library(lm.beta)
+
+ELP <- read.csv("ELP.csv")
+
+str(ELP)
+
+head(ELP)
+
+par(mar = c(5,5,4,2))
+
+plot(ELP$Mean_RT ~ ELP$Length,
+     main = "Scatterplot of Word Lengths and Mean RTs",
+     xlab = "Word Length", ylab = "Mean RT")
+
+par(mfrow = c(1,2))
+
+ELP$LogRT <- log(ELP$Mean_RT)
+
+LengthOutliers <- boxplot.stats(ELP$Length)$out
+
+ELP_1 <- ELP[ELP$Length < 15,]
+
+par(mfrow = c(1,2))
+
+par(mfrow = c(1,1))
+
+LogRTOutliers_1 <- boxplot.stats(ELP_1$LogRT)$out
+
+sort(LogRTOutliers_1)
+
+ELP_2 <- ELP_1[ELP_1$LogRT < 7,]
+
+m <- lm(ELP_2$LogRT ~ ELP_2$Length)
+
+summary(m)
+
+par(mar = c(5,5,4,2))
+
+plot(ELP_2$LogRT ~ ELP_2$Length,
+     main = "Scatterplot of Word Lengths and Log-Transformed RTs",
+     xlab = "Word Length", ylab = "Log-Transformed RT")
+
+abline(m)
